695b8287e8b5ff94bf9df19d8f03cfb0f0632808
basu <basu@basu.com>
2024-06-21 17:14:49 -0700

    login rejister fix

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,10 +1,17 @@
 const mysql = require('mysql2/promise');
 
+// const connection = mysql.createPool({
+//     host: 'localhost',
+//     user: 'terangamain',
+//     password: 'terangamain',
+//     database: 'terangamain',
+// });
 const connection = mysql.createPool({
     host: 'localhost',
     user: 'terangamain',
     password: 'terangamain',
-    database: 'terangamain',
+    database: 'bgdsamm',
 });
-
 export default connection;
+
+
